b1952dec60ae7bcbdaf9e9748b2b868c7ccd3b4c
y-z-f <976691141@qq.com>
2016-04-12 19:15:08 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,11 @@
+#简介
+具体使用方式可以见sample中的项目，后续会补全相关的API说明
+
+##开发环境
+Visual Studio 2015 + Xamarin 4.0.0.1717
+
+##nuget安装
+    Install-Package Sino.Droid.SlidePager
+
+#示例图片
+![](https://github.com/Xamarin-Cn/SlidePager/blob/master/img/1.jpg)
---
+++ SlidePager.Sample/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using Sino.Droid.SlidePager;
+using System.Collections.Generic;
+
+namespace SlidePager.Sample
+{
+    [Activity(Label = "SlidePager.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SlideView sv = new SlideView(this);
+            sv.SetImageResources(Resource.Drawable.welfirst, Resource.Drawable.welsecond, Resource.Drawable.welthird);
+            sv.AttachTo();
+        }
+    }
+}
+
